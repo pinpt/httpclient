085603fb288356ce4abd5f62157c1fad6e41e00f
Jose C Ordaz <cordaz@pinpoint.com>
2019-12-04 11:39:08 -0600

    test

--- httpclient.go
+++ httpclient.go
@@ -70,6 +70,7 @@ func NewHTTPClientDefault() *HTTPClient {
 func NewHTTPClient(ctx context.Context, config *Config, client Client) *HTTPClient {
 	fmt.Println("Just testing the codacy integration")
 	fmt.Println("added new line")
+	fmt.Println("added new line")
 	if config == nil {
 		config = NewConfig()
 	}
